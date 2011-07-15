3dff8ca6d5d26da2b0204872cc8362384ad5a9a2
Shantanu Kumar <kumar.shantanu@gmail.com>
2011-07-16 04:00:41 +0530

    fix typo in project description

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # lein-exec
 
-Leiningen plugin to execute Clojure scripts
+Leiningen plugin to execute Clojure scripts in a project
 
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-exec "0.1"
-  :description "Leiningen plugin to execute a Clojure script"
+  :description "Leiningen plugin to execute Clojure scripts in projects"
   :url "https://github.com/kumarshantanu/lein-exec"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]]
   :eval-in-leiningen true)

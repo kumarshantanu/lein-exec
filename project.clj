dba01f2915dc2ac3f40b3041ff81c65210573506
Shantanu Kumar <kumar.shantanu@gmail.com>
2011-07-16 03:43:12 +0530

    add project URL to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-exec "0.1"
   :description "Leiningen plugin to execute a Clojure script"
+  :url "https://github.com/kumarshantanu/lein-exec"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]]
   :eval-in-leiningen true)
